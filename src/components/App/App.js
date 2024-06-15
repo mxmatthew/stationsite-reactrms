623023c1cc873ebca152f8287e4f48c223d23f92
matthewskills <matt.troughear@gmail.com>
2024-06-15 10:09:51 +0100

    replaces cra bootstrapped app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,25 @@
-
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../Login/Login";
+import Dashboard from "../Dashboard/Dashboard";
+import AuthProvider from "../../hooks/AuthProvider";
+import PrivateRoute from "../../router/route/PrivateRoute";
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+          {/* Other routes */}
+        </Routes>
+      </AuthProvider>
+    </Router>
+  </div>
   );
 }
 
