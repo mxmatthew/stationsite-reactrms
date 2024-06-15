96df0bfc481e4a3905b8c2d39bfb6e7e68249fd4
matthewskills <matt.troughear@gmail.com>
2024-06-15 15:59:46 +0100

    adds primereact

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/soho-light/theme.css";
+import "primeflex/primeflex.css";
+import 'primeicons/primeicons.css';
+
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import Dashboard from "../Dashboard/Dashboard";
@@ -8,18 +14,20 @@ import PrivateRoute from "../../router/route/PrivateRoute";
 function App() {
  
   return (
-    <div className="App">
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Dashboard />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </Router>
+  <div className="App">
+    <PrimeReactProvider>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/" element={<Dashboard />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </PrimeReactProvider>
   </div>
   );
 }
