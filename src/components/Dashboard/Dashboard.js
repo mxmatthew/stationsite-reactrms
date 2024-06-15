d23164eb2dee3194790fe5cdafd740bc61b85eeb
matthewskills <matt.troughear@gmail.com>
2024-06-15 17:20:55 +0100

    adds sign out button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,33 @@
+import { useAuth } from "../../hooks/AuthProvider";
+
+import { PanelMenu } from 'primereact/panelmenu';
+import { PrimeIcons } from 'primereact/api';
+
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const auth = useAuth();
+    const menuItems = [];
 
-    return  <h1>Dashboard</h1>
+    // logout button
+    menuItems.push({
+        label: 'Sign Out',
+        icon: PrimeIcons.SIGN_OUT,
+        command: (e) => {
+            auth.handleLogout();
+        }
+    });
+
+    return  (
+        <div>
+            <div className="flex nested-grid">
+                <nav className="col-fixed bg-primary-reverse h-screen" >
+                    <PanelMenu model={menuItems} className="w-full md:w-20rem p-3"  /> 
+                </nav>
+                <div className="col"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard;
