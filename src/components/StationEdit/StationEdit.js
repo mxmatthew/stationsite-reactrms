044869f2385ff6dbe9f1ca714516f59220e57eb8
matthewskills <matt.troughear@gmail.com>
2024-06-30 08:43:13 +0100

    prevents saving empty station name

--- src/components/StationEdit/StationEdit.js
+++ src/components/StationEdit/StationEdit.js
@@ -17,6 +17,11 @@ function StationEdit (props) {
     }
 
     const handleSaveClick = () => {
+        if (input.name.length < 1) {
+            toast.current.show({severity:'error', summary: 'Invalid Input', detail: 'Please enter a station name and try again', life: 6000});
+            return true;
+        }
+
         StationSite.ModifyStation(input, props.station._id).then((res) => {
             if (res.error) {
                 toast.current.show({severity:'error', summary: 'Failed', detail: res.error, life: 6000});
@@ -40,32 +45,32 @@ function StationEdit (props) {
                     
                 <div className="flex flex-column gap-2 mb-5 w-9">
                     <label htmlFor="stationName">Name</label>
-                    <div className="text-sm">Enter your radio station name</div>
-                    <InputText id="stationName" name="name" onChange={(handleInput)} value={input.name || ''}  />
+                    <InputText id="stationName" name="name" required onChange={(handleInput)} value={input.name || ''} aria-describedby="name-help"  />
+                    <small id="name-help">Enter your radio station name</small>
                 </div>
 
                 <div className="flex flex-column gap-2 mb-5 w-9">
                     <label htmlFor="stationTagline">Tagline</label>
-                    <div className="text-sm">Enter your station's tagline/slogan</div>
-                    <InputText id="stationTagline" name="tagline" onChange={handleInput} value={input.tagline || ''}  />
+                    <InputText id="stationTagline" name="tagline" onChange={handleInput} value={input.tagline || ''} aria-describedby="tagline-help"  />
+                    <small id="tagline-help">Enter your station's tagline/slogan</small>
                 </div>
 
                 <div className="flex flex-column mb-5 gap-2 w-9">
                     <label htmlFor="stationDescription">Description</label>
-                    <div className="text-sm">Enter a description for your radio station</div>
-                    <InputTextarea id="stationDescription" name="description" rows={5} cols={30} onChange={handleInput} value={input.description || ''}   />
+                    <InputTextarea id="stationDescription" name="description" rows={5} cols={30} onChange={handleInput} value={input.description || ''} aria-describedby="description-help" />
+                    <small id="description-help">Enter a description for your radio station</small>
                 </div>
 
                 <div className="flex flex-column mb-5 gap-2 mb-5 w-9">
                     <label htmlFor="stationURL">Website URL</label>
-                    <div className="text-sm">Enter the URL to your station's homepage</div>
-                    <InputText id="stationURL" name="websiteUrl" onChange={handleInput} value={input.websiteUrl || ''}  />
+                    <InputText id="stationURL" name="websiteUrl" onChange={handleInput} value={input.websiteUrl || ''} aria-describedby="websiteUrl-help" />
+                    <small id="websiteUrl-help">Enter the URL to your station's homepage</small>
                 </div>
 
                 <div className="flex flex-column mb-5 gap-2 mb-5 w-9">
                     <label htmlFor="stationStream">Stream URL</label>
-                    <div className="text-sm">Enter the URL to your station's live stream</div>
-                    <InputText id="stationStream" name="streamUrl" onChange={handleInput} value={input.streamUrl || ''}   />
+                    <InputText id="stationStream" name="streamUrl" onChange={handleInput} value={input.streamUrl || ''} aria-describedby="streamUrl-help"  />
+                    <small id="streamUrl-help">Enter the URL to your station's live stream</small>
                 </div>
 
                 </TabPanel>
